8fb607f2a2cd2dfaf327b26b6fe7c0c654777865
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2019-06-29 02:41:51 -0700

    create /task post api to create a new task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose')
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +18,15 @@ app.post('/user', (req, res) => {
     })
 })
 
+app.post('/task', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.send(task)
+    }
+    ).catch((e) => {
+        res.status(400).send(e)
+    })
+})
 
 app.listen(port, () => {
     console.log('Server is running on port' + port);    
