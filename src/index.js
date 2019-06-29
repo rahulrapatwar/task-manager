f24e6b6b8b60647a0ed28543b502fc1b71d96ff6
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2019-06-29 02:23:00 -0700

    created /user post api to create a new user

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('./db/mongoose')
+const User = require('./models/user');
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/user', (req, res) => {
+    const user = new User(req.body)
+    user.save().then((result) => {
+        res.send(result)
+    }
+    ).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('Server is running on port' + port);    
+})
