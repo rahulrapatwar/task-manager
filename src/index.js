586d7cd7035acc5328b2612a0a01653333a3c6ea
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2019-06-30 23:55:40 -0700

    integrated async/await in all apis

--- src/index.js
+++ src/index.js
@@ -8,64 +8,68 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/user', (req, res) => {
+app.post('/user', async (req, res) => {
     const user = new User(req.body)
-    user.save().then((result) => {
-        res.send(result)
-    }
-    ).catch((error) => {
+    try {
+        await user.save()
+        res.send(user)
+    } catch (error) {
         res.status(400).send(error)
-    })
-})
-
-app.post('/task', (req, res) => {
-    const task = new Task(req.body)
-    task.save().then(() => {
-        res.send(task)
     }
-    ).catch((e) => {
-        res.status(400).send(e)
-    })
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.post('/task', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.send(task)
+    } catch (error) {
+        res.status(400)
+    }
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
         res.send(users)
-    }).catch((e) => {
+    } catch (error) {
         res.status(500).send('Internal server error')
-    })
+    }
 })
 
-app.get('/user/:id', (req, res) => {
+app.get('/user/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+    try {
+        const user = await User.findById(_id)
         if (!user) {
             return res.status(404).send('User not found')
         }
         res.send(user)
-    }).catch((e) => {
+    } catch (error) {
         res.status(500).send('Internal server error')
-    })
+    }
 })
 
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e) => {
+    } catch (error) {
         res.status(500).send('Internal server error')
-    })
+    }
 })
 
-app.get('/task/:id', (req, res) => {
+app.get('/task/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
+    try {
+        const task = await Task.findById(_id)
         if (!task) {
             return res.status(404).send('Task not found')
         }
         res.send(task)
-    }).catch((e) => {
+    } catch (error) {
         res.status(500).send('Internal server error')
-    })
+    }
 })
 
 app.listen(port, () => {
