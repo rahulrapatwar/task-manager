843944d355b29726646545e5fdf8c95500efdfdb
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2019-07-01 23:45:34 -0700

    refactor /users api and add auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = new express.Router()
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
-
+// Create new user
 router.post('/user', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.status(200).send({user, token})
     } catch (error) {
         res.status(400).send(error)
     }
@@ -24,13 +26,8 @@ router.post('/user/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (error) {
-        res.status(500).send('Internal server error')
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/user/:id', async (req, res) => {
