c4a88dc74bb1ac787fc480ab6136265c4c059cf3
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2019-07-02 03:10:17 -0700

    add time stamps to user and task model

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('../models/task');
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -43,6 +44,8 @@ const userSchema = mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 })
 
 // Add toJSON to userSchema to hide private data
@@ -83,6 +86,12 @@ userSchema.pre('save', async function (next) {
         user.password = await bcrypt.hash(user.password, 8)
     }
     next()
+})
+
+// Delete all tasks for the user to be removed
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({owner: user._id})
 })
 
 const User = mongoose.model('User', userSchema)
