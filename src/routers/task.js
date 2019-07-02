e80910511709c5206d3a04379b450457d1915d11
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2019-07-02 01:57:56 -0700

    add auth and refactor all task apis

--- src/routers/task.js
+++ src/routers/task.js
@@ -16,19 +16,19 @@ router.post('/task', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
+        const tasks = await Task.find({owner: req.user._id})
         res.send(tasks)
     } catch (error) {
         res.status(500).send('Internal server error')
     }
 })
 
-router.get('/task/:id', async (req, res) => {
+router.get('/task/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({_id, owner: req.user._id})
         if (!task) {
             return res.status(404).send('Task not found')
         }
@@ -38,7 +38,7 @@ router.get('/task/:id', async (req, res) => {
     }
 })
 
-router.patch('/task/:id', async (req, res) => {
+router.patch('/task/:id', auth, async (req, res) => {
     const _id = req.params.id
     const inputFields = Object.keys(req.body)
     const allowedFields = ['description', 'completed']
@@ -48,24 +48,24 @@ router.patch('/task/:id', async (req, res) => {
         return res.status(400).send('Invalid fields in request body')
     }
     try {
-        const task = await Task.findById(_id)
-
-        inputFields.forEach((field) => task[field] = req.body[field])
-        await task.save()
+        const task = await Task.findOne({_id, owner: req.user._id})
 
         if (!task) {
             return res.status(404).send()
         }
+
+        inputFields.forEach((field) => task[field] = req.body[field])
+        await task.save()
         res.send(task)
     } catch (error) {
         res.status(500).send(error)
     }
 })
 
-router.delete('/task/:id', async (req, res) => {
+router.delete('/task/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findByIdAndDelete(_id)
+        const task = await Task.findOneAndDelete({_id, owner: req.user._id})
         if (!task) {
             return res.status(404).send()
         }
